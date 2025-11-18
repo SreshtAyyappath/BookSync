32084775af86e9b5746aeac93b22ae1ee0780187
sresht.ayyappath <sresht.ayyappath@tanla.com>
2025-11-18 18:07:13 +0530

    added postgres yaml and backend start

---
+++ backend/booksync/src/main/java/com/sresht/booksync/BooksyncApplication.java
@@ -0,0 +1,13 @@
+package com.sresht.booksync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksyncApplication.class, args);
+	}
+
+}
---
+++ backend/booksync/src/test/java/com/sresht/booksync/BooksyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sresht.booksync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
